35afef142f002ce4c5f32435058ea9ccb9ec8ef2
ShiroKoNeko-c <webbelajar886@gmail.com>
2025-09-15 18:03:46 +0700

    pertemuan2

--- aplikasi2/app.js
+++ aplikasi2/app.js
@@ -1,12 +1,55 @@
-// app.js
-const http = require('http');
+// // app.js
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello, World!\n');
+// });
+
+// server.listen(3000, () => {
+//     console.log('Server running at http://localhost:3000/');
+// });
+
+// app.js
+
+// Route GET
+app.get('/', (req, res) => {
+  res.send('Hello, GET request!');
 });
 
-server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000/');
+// Middleware untuk parsing body request
+app.use(express.json());
+
+// Route POST
+app.post('/submit', (req, res) => {
+  const { name } = req.body;
+  res.send(`Hello, ${name}!`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+app.get('/text', (req, res) => {
+  res.send('Ini adalah response text.');
+});
+
+app.get('/html', (req, res) => {
+  res.send('<h1>Ini adalah response HTML</h1>');
+});
+
+app.get('/json', (req, res) => {
+  res.json({ message: 'Ini adalah response JSON' });
+});
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Menyajikan file statis dari folder 'public'
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
