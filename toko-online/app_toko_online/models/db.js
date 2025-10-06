aea4fab83a486050e72e4929542391ec9471c328
ShiroKoNeko-c <why2301p@gmail.com>
2025-10-06 20:26:22 +0700

    Update db.js

--- toko-online/app_toko_online/models/db.js
+++ toko-online/app_toko_online/models/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-const dbURI = "mongodb://localhost:27017/paw2";
+const dbURI = "mongodb://localhost:27017/paw2-si5c";
 
 mongoose.connect(dbURI, {});
 
 mongoose.connection.on("connected", () => {
-  console.log('Mongoose connected to ${dbURI}');
+  console.log(`Mongoose connected to ${dbURI}`);
 });
 
 mongoose.connection.on("error", (err) => {
